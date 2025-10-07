d06b797f59474edf84b2e53f0f01551c69b7047e
Nicolas Melichopulos <nicolas.m91@live.com.ar>
2025-10-07 00:44:29 -0300

    mapping pokemon types

---
+++ src/main/java/com/otsnd/pokemonteambuilder/dtos/TypesDTO.java
@@ -0,0 +1,13 @@
+package com.otsnd.pokemonteambuilder.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TypesDTO {
+    private Integer slot;
+    private TypeItemDTO type;
+}
