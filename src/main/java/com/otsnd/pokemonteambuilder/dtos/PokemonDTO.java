7a57d84d691e79874fbdf9cd321621dcf7be4b80
Rosina-cristaldo <rosinacristaldo@gmail.com>
2025-10-21 20:23:20 -0300

    c:

--- src/main/java/com/otsnd/pokemonteambuilder/dtos/PokemonDTO.java
+++ src/main/java/com/otsnd/pokemonteambuilder/dtos/PokemonDTO.java
@@ -2,13 +2,17 @@ package com.otsnd.pokemonteambuilder.dtos;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Data
 @JsonDeserialize
+@NoArgsConstructor
 public class PokemonDTO {
     private Long id;
     private String name;
     private Integer order;
+    private List<TypesDTO> types;
 }
 
 
